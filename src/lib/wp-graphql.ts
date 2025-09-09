261cb4eb042a4ba86cc641471b86124fd0adb4bf
nomucode <noelmrodri@gmail.com>
2025-09-10 00:58:26 +0200

    Added forms

---
+++ src/lib/wp-graphql.ts
@@ -0,0 +1,70 @@
+const API_URL = import.meta.env.PUBLIC_WORDPRESS_API_URL;
+
+async function fetchAPI(query = "", variables: Record<string, any> = {}) {
+  const headers = { "Content-Type": "application/json" };
+
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  const json = await res.json();
+
+  if (json.errors) {
+    console.error(json.errors);
+    throw new Error("Failed to fetch API");
+  }
+
+  return json.data;
+}
+
+export async function getAllObras() {
+  const data = await fetchAPI(`
+    query GetAllObras {
+      obras(first: 100) { 
+        nodes {
+          title
+          slug
+          featuredImage {
+            node {
+              sourceUrl
+              altText
+            }
+          }
+        }
+      }
+    }
+  `);
+  return data?.obras.nodes;
+}
+
+export async function getObraBySlug(slug: string) {
+  const data = await fetchAPI(
+    `
+    query GetObraBySlug($id: ID!) {
+      obra(id: $id, idType: SLUG) {
+        title
+        content
+        featuredImage {
+          node {
+            sourceUrl
+            altText
+          }
+        }
+        datosDeLaObra {
+          descripcion
+          ano
+        }
+      }
+    }
+  `,
+    {
+      id: slug,
+    }
+  );
+  return data?.obra;
+}
--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -4,17 +4,21 @@ const domain = import.meta.env.PUBLIC_WP_DOMAIN;
 const apiUrl = `${domain}/wp-json/wp/v2`;
 
 export const getPageInfo = async (slug: string) => {
-  const response = await fetch(`${apiUrl}/pages?slug=${slug}`);
+  const response = await fetch(`${apiUrl}/pages?slug=${slug}&_embed`);
 
   if (!response.ok) {
     throw new Error("Failed fetching data from WP");
   }
 
   const [data] = await response.json();
+
   const {
     title: { rendered: title },
     content: { rendered: content },
   } = data;
 
-  return { title, content };
+  const featuredImageUrl =
+    data._embedded?.["wp:featuredmedia"]?.[0]?.source_url || null;
+
+  return { title, content, featuredImageUrl };
 };
